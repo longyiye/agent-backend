a5892e75942ed520de0bf733a026cc4e2012a934
龙一野 <2226548812@qq.com>
2025-09-02 23:07:12 +0800

    基于PGVector实现向量存储

---
+++ src/test/java/com/yiye/rag/PgVectorVectorStoreConfigTest.java
@@ -0,0 +1,32 @@
+package com.yiye.rag;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Map;
+
+@SpringBootTest
+public class PgVectorVectorStoreConfigTest {
+
+    @Resource
+    VectorStore pgVectorVectorStore;
+
+    @Test
+    void test() {
+        List<Document> documents = List.of(
+                new Document("小野狼是谁？小野狼喜欢谁？", Map.of("meta1", "meta1")),
+                new Document("小野狼又叫做棒棒糖"),
+                new Document("小野狼这小伙子比较帅气", Map.of("meta2", "meta2")));
+        // 添加文档
+        pgVectorVectorStore.add(documents);
+        // 相似度查询
+        List<Document> results = pgVectorVectorStore.similaritySearch(SearchRequest.builder().query("棒棒糖").topK(3).build());
+        Assertions.assertNotNull(results);
+    }
+}
